119ce83e28d7ea3f2022487c01d6164637f8f04f
Emil Schallwig <emil.schallwig@gmail.com>
2023-01-20 12:12:19 +0000

    Experimenting with the sort function. task 3-8

--- week1.py
+++ week1.py
@@ -1,22 +1,14 @@
-guests = ["Anna", "Hannes", "Felix"]
-personleft = "Anna"
-personadded = "Mick"
-guests.remove(personleft)
-guests.append(personadded)
-for x in guests:
-    message = f"Hi {x.title()}.{personleft} sadly can't make it... However, I found a bigger table and I will invite more people"
-    print(message)
-
-guests.insert(0,"Lennox")
-guests.insert(int(len(guests)/2),"Lennart")
-guests.append("Omar")
-
-for x in guests:
-    message = f"Hi {x.title()}. You are invited to my dinenr party!"
-    message += f"Damn, I just found out the table won't arrive in time. I have to shrink the list..."
-    print(message)
-
-for x in range(len(guests)):
-    if len(guests) > 2:
-        shrinkedperson = guests.pop()
-        print(f"Sorry, {shrinkedperson}. Because i dont't have any space I have to remove you from the list...")
+locations = ["London", "Bali", "California"]
+print(locations)
+print(sorted(locations))
+print(locations)
+print(sorted(locations,reverse = True))
+print(locations)
+locations.reverse()
+print(locations)
+locations.reverse()
+print(locations)
+locations.sort()
+print(locations)
+locations.sort(reverse=True)
+print(locations)
