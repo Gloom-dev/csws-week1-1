b1af41a5773182d016ad7bbc2ada4b8dcd7ebdd7
Emil Schallwig <emil.schallwig@gmail.com>
2023-01-20 12:23:29 +0000

    Printing length of a list. Task 3-9

--- week1.py
+++ week1.py
@@ -1,14 +1,2 @@
-locations = ["London", "Bali", "California"]
-print(locations)
-print(sorted(locations))
-print(locations)
-print(sorted(locations,reverse = True))
-print(locations)
-locations.reverse()
-print(locations)
-locations.reverse()
-print(locations)
-locations.sort()
-print(locations)
-locations.sort(reverse=True)
-print(locations)
+guests = ["Anna", "Hannes", "Felix"]
+print(len(guests))
