a38eabed074b0fcf0ad0e4c9041ebe19f5b8f9e7
Emil Schallwig <emil.schallwig@gmail.com>
2023-01-20 11:55:04 +0000

    Adding more guests. Crash course 3-6

--- week1.py
+++ week1.py
@@ -4,6 +4,13 @@ personadded = "Mick"
 guests.remove(personleft)
 guests.append(personadded)
 for x in guests:
+    message = f"Hi {x.title()}.{personleft} sadly can't make it... However, I found a bigger table and I will invite more people"
+    print(message)
+
+guests.insert(0,"Lennox")
+guests.insert(int(len(guests)/2),"Lennart")
+guests.append("Omar")
+
+for x in guests:
     message = f"Hi {x.title()}. You are invited to my dinenr party!"
     print(message)
-print(f"{personleft} can't make it...")
